c6fee6ae83c12843948616ac44861343238168a5
Егор Кучук <kriptyashka@gmail.com>
2023-08-25 21:24:48 +0300

    Многопоточность, сученьки!
    Смотреть на 187-196 и 97 строки

--- Interface21.py
+++ Interface21.py
@@ -1,17 +1,13 @@
-
 import sys
+import random
 import threading
-import time
-import random 
+from time import sleep
+from multiprocessing import Process
 from PyQt5.QtWidgets import *
-from PyQt5.QtWidgets import QMessageBox, QApplication, QPushButton, QWidget
-from PyQt5 import QtCore, QtGui, QtWidgets, uic 
-from time import time, sleep
-from PyQt5.QtGui import QIcon, QFont, QPixmap
-from PyQt5.QtCore import QCoreApplication, QBasicTimer, QDateTime, Qt, QSize, QTimer
+from PyQt5.QtCore import QCoreApplication, QBasicTimer, QDateTime, Qt, QSize, QTimer, QThread
 import Interface
-
 from DialWind import Ui_Dialog
+
 
 class RegulWindow(QDialog, Ui_Dialog):
     def __init__(self, root):
@@ -20,7 +16,7 @@ class RegulWindow(QDialog, Ui_Dialog):
         self.MainWindow = root
         self.pushButton_13.clicked.connect(self.acept_data)
         self.pushButton_14.clicked.connect(self.reject_data)
-        
+
         self.pushButton_1.clicked.connect(lambda: self.add_text("1"))
         self.pushButton_2.clicked.connect(lambda: self.add_text("2"))
         self.pushButton_3.clicked.connect(lambda: self.add_text("3"))
@@ -37,7 +33,7 @@ class RegulWindow(QDialog, Ui_Dialog):
     def add_text(self, text):
         current_text = self.label_value_flow_Ar.text()
         self.label_value_flow_Ar.setText(current_text + text)
-        
+
     def clear_text(self):
         self.label_value_flow_Ar.setText("")
 
@@ -49,6 +45,7 @@ class RegulWindow(QDialog, Ui_Dialog):
         print('команда сработала')
         self.close()
 
+
 class RegulWindow2(QDialog, Ui_Dialog):
     def __init__(self, root):
         QDialog.__init__(self, root)
@@ -56,7 +53,7 @@ class RegulWindow2(QDialog, Ui_Dialog):
         self.MainWindow = root
         self.pushButton_13.clicked.connect(self.acept_data)
         self.pushButton_14.clicked.connect(self.reject_data)
-        
+
         self.pushButton_1.clicked.connect(lambda: self.add_text("1"))
         self.pushButton_2.clicked.connect(lambda: self.add_text("2"))
         self.pushButton_3.clicked.connect(lambda: self.add_text("3"))
@@ -73,7 +70,7 @@ class RegulWindow2(QDialog, Ui_Dialog):
     def add_text(self, text):
         current_text = self.label_value_flow_Ar.text()
         self.label_value_flow_Ar.setText(current_text + text)
-        
+
     def clear_text(self):
         self.label_value_flow_Ar.setText("")
 
@@ -84,14 +81,21 @@ class RegulWindow2(QDialog, Ui_Dialog):
     def reject_data(self):
         self.close()
 
+
+def click_openAr():
+    global current_command
+    current_command = "020F000200020101"
+    print("def click_openAr выполнено")
+
+
 class MainWindow(QMainWindow, Interface.Ui_MainWindow):
     def __init__(self, parent=None):
         super(MainWindow, self).__init__()
         self.setupUi(self)
         self.flow_value = ' '
-        
-        self.btn_openO.clicked.connect(self.click_openO) #функции нажатия на кнопки
-        self.btn_openAr.clicked.connect(self.click_openAr)
+
+        self.btn_openO.clicked.connect(self.thread_openO)  # функции нажатия на кнопки
+        self.btn_openAr.clicked.connect(click_openAr)
         self.btn_closeO.clicked.connect(self.click_closeO)
         self.btn_closeAr.clicked.connect(self.click_closeAr)
         self.btn_regulateO.clicked.connect(self.click_regulateO)
@@ -100,20 +104,20 @@ class MainWindow(QMainWindow, Interface.Ui_MainWindow):
         self.btn_installAr.clicked.connect(self.click_installAr)
         self.fakeButtonO.clicked.connect(self.show_keyboard_dialogO)
         self.fakeButtonO_2.clicked.connect(self.show_keyboard_dialogAr)
-        self.pushButton.clicked.connect(self.Exit_)                   # клик на кнопку ВЫХОД
+        self.pushButton.clicked.connect(self.Exit_)  # клик на кнопку ВЫХОД
 
-        #GPIO.setmode(GPIO.BCM)                                  # Инициализация пина RPi - Master (используется при MAX485)
-        #GPIO.setup(DU_Pin_Rpi_Master, GPIO.OUT)
-        #GPIO.output(DU_Pin_Rpi_Master, True)
-        #self.setGeometry(0, 70, 1024, 600)                        # расположение главновго окна
-        self.setWindowFlags(Qt.FramelessWindowHint)             # убирает шапку приложения
-        #icon_switch_off = QtGui.QIcon()                         # картинка на кнопке ВЫХОД
-        #icon_switch_off.addPixmap(QtGui.QPixmap("/home/pi/Desktop/Modbus/close.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
-        #self.Exit.setIcon(icon_switch_off)                      # добавляет иконку
-        
-    def Exit_(self):                                         # при выходе из программы
+        # GPIO.setmode(GPIO.BCM)                                  # Инициализация пина RPi - Master (используется при MAX485)
+        # GPIO.setup(DU_Pin_Rpi_Master, GPIO.OUT)
+        # GPIO.output(DU_Pin_Rpi_Master, True)
+        # self.setGeometry(0, 70, 1024, 600)                        # расположение главновго окна
+        self.setWindowFlags(Qt.FramelessWindowHint)  # убирает шапку приложения
+        # icon_switch_off = QtGui.QIcon()                         # картинка на кнопке ВЫХОД
+        # icon_switch_off.addPixmap(QtGui.QPixmap("/home/pi/Desktop/Modbus/close.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        # self.Exit.setIcon(icon_switch_off)                      # добавляет иконку
+
+    def Exit_(self):  # при выходе из программы
         global current_command
-        current_command = 'Exit'   
+        current_command = 'Exit'
 
     def show_keyboard_dialogO(self):
         dialog = RegulWindow(self)
@@ -125,100 +129,103 @@ class MainWindow(QMainWindow, Interface.Ui_MainWindow):
         dialog.show()
         dialog.exec()
 
-    def showEvent(self, event): #запускает программу при при её открытии 
-        global current_command 
+    def showEvent(self, event):  # запускает программу при при её открытии
+        global current_command
         print('Продукт Vasiliev&Vasiliev Embedded Software Technology. Все права защищены')
         current_command = '010300040002'
-        self.start_readout()
+        self.check_o()
 
-    def start_readout(self):
-        global current_command 
-        a = '010300040002'
+    def check_o(self):
+        global current_command
         if current_command == 'Exit':
             sleep(2)
             self.close()
+            return None
+        cmd_show_oxygen = '010300040002'
+        if current_command != cmd_show_oxygen and current_command != 'Exit':
+            print('модуль получил глобальную команду')
+            thread = threading.Thread(target=self.fn_sendcmd, args=(current_command,))
+            thread.start()
+            thread.join()
+            current_command = cmd_show_oxygen
         else:
-            if current_command != a and current_command != 'Exit':
-                print('модуль получил глобальную команду')
-                self.thread1 = threading.Thread(target = self.fn_sendcmd, args=(current_command,) )
-                self.thread1.start()
-                self.thread1.join()
-                current_command = '010300040002'
-            else:
-                print('modul 1 made/ ')
-                self.thread1 = threading.Thread(target = self.fn_sendcmd, args=(a,) )
-                self.thread1.start()
-                self.thread1.join()
-                self.updatelabeltextO(self.flow_value)
-            self.timer = threading.Timer(0.5, self.second_part, args=(str,))
-            self.timer.start()
- 
-    def second_part(self, str):
+            print('modul 1 made/ ')
+            thread = threading.Thread(target=self.fn_sendcmd, args=(cmd_show_oxygen,))
+            thread.start()
+            thread.join()
+            self.updatelabeltextO(self.flow_value)
+
+        timer = threading.Timer(0.5, self.check_ar, args=(str,))
+        timer.start()
+
+    def check_ar(self):
         a = '020300040002'
         print('modul 2 made /')
-        self.thread1 = threading.Thread(target = self.fn_sendcmd, args=(a,) )
-        self.thread1.start()
-        self.thread1.join()
+        thread = threading.Thread(target=self.fn_sendcmd, args=(a,))
+        thread.start()
+        thread.join()
         self.updatelabeltextAr(self.flow_value)
-        self.start_readout()
+        self.check_o()
 
-    def updatelabeltextAr(self, number):
-        number = str(round(number/1.45, 2))
-        self.label_realflowAr.setText(number)
+    def updatelabeltextAr(self, str_num):
+        str_num = str(round(str_num / 1.45, 2))
+        self.label_realflowAr.setText(str_num)
 
-    def updatelabeltextO(self, number):
-        number = str(round(number, 2))
-        self.label_realflowO.setText(number)
+    def updatelabeltextO(self, str_num):
+        str_num = str(round(str_num, 2))
+        self.label_realflowO.setText(str_num)
 
-    def fn_sendcmd(self, number):                                       # извлекаем содержимое ячеек
-        print("def fn_sendcmd получило значение - ", number)                         # данные
-        self.ed_id= number[0:2]                           # адрес устройства ID
-        self.ed_cmd=number[2:4]                           # номер команды
-        self.ed_adr=number[4:8]                           # адрес регистра
-        self.ed_count=number[8:17]                          # данные
-        self.flow_value = random.randint(0,100)
+    def fn_sendcmd(self, number):  # извлекаем содержимое ячеек
+        print("def fn_sendcmd получило значение - ", number)  # данные
+        ed_id = number[0:2]  # адрес устройства ID
+        ed_cmd = number[2:4]  # номер команды
+        ed_adr = number[4:8]  # адрес регистра
+        ed_count = number[8:17]  # данные
+        self.flow_value = random.randint(0, 100)
         sleep(1)
 
     def click_openO(self):
-        global current_command  
+        global current_command
         current_command = "010F000200020101"
         print("def click_openO выполнено")
+        for i in range(1_000_000):
+            print(i)
 
-    def click_openAr(self):
-        global current_command  
-        current_command = "020F000200020101"
-        print("def click_openAr выполнено")
-        
-    def click_closeO(self):
-        global current_command  
+    def thread_openO(self):
+        t1 = threading.Thread(target=self.click_openO)
+        t1.start()
+
+    @staticmethod
+    def click_closeO():
+        global current_command
         current_command = "010F000200020102"
         print("def click_closeO выполнено")
 
     def click_closeAr(self):
-        global current_command  
+        global current_command
         current_command = "020F000200020102"
         print("def click_closeAr выполнено")
-        
+
     def click_regulateO(self):
-        global current_command  
+        global current_command
         current_command = "010F000200020100"
         print("def click_regulateO выполнено")
-        
+
     def click_regulateAr(self):
-        global current_command  
+        global current_command
         current_command = "020F000200020100"
         print("def click_regulateAr выполнено")
-        
+
     def click_installO(self):
-        value_flow_1 = self.fakeLineEditO.text() #значение из TextEdit в строку
-        try:
+        value_flow_1 = self.fakeLineEditO.text()  # значение из TextEdit в строку
+        try:  # TODO: Слишком много строк, где можно завалиться. Надо уменьшить try, а лучше от него избавиться
             value_flow_1 = float(value_flow_1)
-            procent = int((value_flow_1/90)*10000*1.45)
+            procent = int((value_flow_1 / 90) * 10000 * 1.45)
             procent1 = hex(procent)
             if value_flow_1 > 90:
                 self.show_error("Значение должно быть не больше 90 л/с")
             else:
-                procent1=str(procent1)
+                procent1 = str(procent1)
                 print("отчивка", procent1)
                 if len(procent1) < 6:
                     procent2 = "0" + procent1[2:6]
@@ -228,20 +235,20 @@ class MainWindow(QMainWindow, Interface.Ui_MainWindow):
                 print("def click_installO выполнено", procent2)
                 type_command = "01060004" + procent2
                 print(type_command)
-                global current_command  
+                global current_command
                 current_command = type_command
-        except: 
+        except:
             self.show_error('Введено некорректное значение потока')
 
     def click_installAr(self):
-        value_flow_1 = self.fakeLineEditO_2.text() #значение из TextEdit в строку
+        value_flow_1 = self.fakeLineEditO_2.text()  # значение из TextEdit в строку
         try:
             value_flow_1 = float(value_flow_1)
-            procent = int((value_flow_1/90)*10000*1.45)
+            procent = int((value_flow_1 / 90) * 10000 * 1.45)
             procent1 = hex(procent)
             if value_flow_1 > 90:
                 self.show_error("Значение должно быть не больше 90 л/с")
-                procent1=str(procent1)
+                procent1 = str(procent1)
                 print("отчивка", procent1)
                 if len(procent1) < 6:
                     procent2 = "0" + procent1[2:6]
@@ -251,13 +258,12 @@ class MainWindow(QMainWindow, Interface.Ui_MainWindow):
                     print("def click_installO выполнено", procent2)
                     type_command = "02060004" + procent2
                     print(type_command)
-                    global current_command  
+                    global current_command
                     current_command = type_command
-        except: 
+        except:
             self.show_error('Введено некорректное значение потока')
 
-
-    def show_error(self, str): #вывод ошибки 
+    def show_error(self, str):  # вывод ошибки
         error = QMessageBox()
         error.setWindowTitle("Ошибка")
         error.setText(str)
@@ -266,10 +272,11 @@ class MainWindow(QMainWindow, Interface.Ui_MainWindow):
         error.exec()
 
 
-def main():                                                     # открытие главного окна
+def main():  # открытие главного окна
     app = QApplication(sys.argv)
     main_win = MainWindow()
     main_win.show()
     sys.exit(app.exec_())
+
 
 if __name__ == "__main__": main()
